545a2dddee3d98a05e0f214b20bd5607a749c7b3
Arthur Talla <ggtalla27@gmail.com>
2024-06-03 12:56:53 +0100

    mise a jour des test 1

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,4 +18,11 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
+
+    public function testBasicTest2()
+    {
+        $response = $this->get('/a');
+
+        $response->assertStatus(400);
+    }
 }
